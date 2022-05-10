2f4e1cdeb5ccb3c5b58a40fad3cdebd0d51d43b5
djatjdwns28 <djatjdwns28@gmail.com>
2022-05-10 21:40:54 +0900

    UserService 패스워드 암호화 적용

--- src/main/java/com/sign/signupandsignin/api/service/UserService.java
+++ src/main/java/com/sign/signupandsignin/api/service/UserService.java
@@ -5,6 +5,7 @@ import com.sign.signupandsignin.api.entity.UserEntity;
 import com.sign.signupandsignin.api.mapper.UserMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,13 +15,17 @@ public class UserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
     @Transactional
     public Integer createUser(
         UserDto userDto
     ) {
         UserEntity userEntity = UserEntity.builder()
             .userId(userDto.getUserId())
-            .password(userDto.getPassword())
+            .password(passwordEncoder.encode(userDto.getPassword()))
             .age(userDto.getAge())
             .sex(userDto.getSex())
             .phoneNum(userDto.getPhoneNum())
